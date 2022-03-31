85758b0834b87e74ce2962d79fb433dbbc00d700
2mnguyen1 <2mnguyen1@gmail.com>
2022-03-31 16:48:09 -0700

    adding first page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import blobs1 from './images/blobs1.png'
+import blobs2 from './images/blobs2.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [start, setStart] = useState(true)
+    
+
+
+
+    return (
+        <main>
+            {start && <div className="container-start">
+                <h1 className="title">Quizzical</h1>
+                <p className="description">Ai thông minh hơn học sinh lớp 5</p>
+                <button onClick={() => setStart(prev => !prev)} className="btn-start">Start quiz</button>
+            </div>}
+            <div className="img-background img-top">
+                <img src={blobs1} alt="top-img"/>
+            </div>
+            <div className="img-background img-bottom">
+                <img src={blobs2} alt="top-img"/>
+            </div>
+        </main>
+    )
 }
-
-export default App;
